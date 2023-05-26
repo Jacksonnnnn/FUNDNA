9fb6932724722bbe937dff96610bfa14941c38ea
Jacksonnnnn <jhuse01@gmail.com>
2023-05-26 15:38:38 -0400

    Nuskell Testing

---
+++ build/NuskellTest.py
@@ -0,0 +1,146 @@
+import nuskell.dsdcompiler
+
+
+def TestTubeAnalysis(testTube, file, replace):
+    DefineTranslation(testTube, file, replace)
+    DetermineDotParensPlus(testTube, file, replace)
+    ListReactions(testTube, file, replace)
+
+
+def ListReactions(testTube, f, replace):
+    f.write("\n\n")
+    f.write('-' * 50)
+    f.write("\nReactions:\n")
+    f.write('-' * 50)
+    f.write("\n")
+
+    i = 0
+    for reaction in testTube[1]:
+        f.write("\tReaction " + str(i) + " - Involved Complexes:\n")
+        for id in reaction:
+            complex = reaction[id]
+            if replace:
+                f.write("\t  - " + complex.name.replace('c', '0.').replace('a', '_0').replace('b', '_1') + "\n")
+            else:
+                f.write("\t  - " + complex.name + "\n")
+        i += 1
+
+
+def DefineTranslation(testTube, f, replace):
+    f.write("\n\n")
+    f.write('-' * 50)
+    f.write("\nTRANSLATION:\n")
+    f.write('-' * 50)
+    for id in testTube[0]:
+        strand = testTube[0][id]
+        if replace:
+            f.write("\nComplex: " + str(strand.name).replace('c', '0.').replace('a', '_0').replace('b', '_1'))
+        else:
+            f.write("\nComplex: " + str(strand.name))
+        f.write("\n\tEnclosed Domains:\t" + " ".join(map(str, strand.enclosed_domains)))
+        f.write("\n\tExterior Domains:\t" + " ".join(map(str, strand.exterior_domains)))
+        if strand.concentration is not None:
+            f.write("\n\tConcentration:\t\t" + " ".join(map(str, strand.concentration)))
+        else:
+            f.write("\n\tConcentration:\t\tUndefined")
+        f.write("\n\tKernel String:\t\t" + str(strand.kernel_string))
+        f.write("\n\tDot-parens-plus:\t" + "".join(strand.structure))
+        pairTable = list(strand.pair_table)
+        f.write("\n\tPair Table:\t")
+        for set in pairTable:
+            f.write("\n\t\t" + str(list(set)))
+
+        strandTable = list(strand.strand_table)
+        f.write("\n\tStrand Table:\t")
+        f.write("\n\t\tDomain:\tLength:")
+        f.write("\n\t\t-------\t-------")
+
+        for strands in strandTable:
+            for domain in strands:
+                f.write("\n\t\t  " + domain.name + "\t  " + str(domain.length))
+
+        f.write("\n")
+
+
+def DetermineDotParensPlus(testTube, f, replace):
+    f.write("\n\n")
+    f.write('-' * 50)
+    f.write("\nDOT-PARENS-PLUS NOTATION OF COMPLEXES:\n")
+    f.write('-' * 50)
+    for id in testTube[0]:
+        strand = testTube[0][id]
+        if replace:
+            f.write("\n" + str(strand.name.replace('c', '0.').replace('a', '_0').replace('b', '_1')) + "\n\t" + "".join(strand.structure) + "\n\t" + str(strand.kernel_string))
+        else:
+            f.write("\n" + str(strand.name) + "\n\t" + "".join(strand.structure) + "\n\t" + str(strand.kernel_string))
+
+
+# CRN Format:
+#       # A list of reactions, optionally with reaction rates:
+#       # <- this is a comment!
+#       B + B -> C    # [k = 1]
+#       C + A <=> D   # [kf = 1, kr = 1]
+#       <=> A  [kf = 15, kr = 6]
+#
+#       # Note that you can write multiple reactions in one line:
+#       A + 2C -> E [k = 13.78]; E + F <=> 2A  [kf = 13, kr = 14]
+
+simpleReactionFile = open("tests/simple.txt", "w+")
+simpleReaction = nuskell.dsdcompiler.translate('A + B -> C', 'soloveichik2010.ts', modular=True)
+TestTubeAnalysis(simpleReaction, simpleReactionFile, False)
+simpleReactionFile.close()
+
+andGateFile = open("tests/andGate.txt", "w+")
+andGate = nuskell.dsdcompiler.translate('A0 + B0 -> C0;'
+                                        'A0 + B1 -> C0;'
+                                        'A1 + B0 -> C0;'
+                                        'A1 + B1 -> C1', 'soloveichik2010.ts', modular=True)
+TestTubeAnalysis(andGate, andGateFile, False)
+andGateFile.close()
+
+# x^2/10 + x/15 + 4/5
+
+# '0.75_0 + X_0 -> G1_1;'
+# '0.75_0 + X_1 -> G1_1;'
+# '0.75_1 + X_0 -> G1_1;'
+# '0.75_1 + X_1 -> G1_0;'
+
+# 'G1_0 + 0.6667_0 -> G2_1;'
+# 'G1_0 + 0.6667_1 -> G2_1;'
+# 'G1_1 + 0.6667_0 -> G2_1;'
+# 'G1_1 + 0.6667_1 -> G2_0;'
+
+# 'G2_0 + G1_0 -> G3_1;'
+# 'G2_0 + G1_1 -> G3_1;'
+# 'G2_1 + G1_0 -> G3_1;'
+# 'G2_1 + G1_1 -> G3_0;'
+
+# 'G3_0 + 0.2_0 -> G4_1;'
+# 'G3_0 + 0.2_1 -> G4_1;'
+# 'G3_1 + 0.2_0 -> G4_1;'
+# 'G3_1 + 0.2_1 -> G4_0'
+polynomialFile = open("tests/polynomial.txt", "w+")
+polynomial = nuskell.dsdcompiler.translate('0.75_0 + X_0 -> G1_1;'
+                                           '0.75_0 + X_1 -> G1_1;'
+                                           '0.75_1 + X_0 -> G1_1;'
+                                           '0.75_1 + X_1 -> G1_0;'
+
+                                           'G1_0 + 0.6667_0 -> G2_1;'
+                                           'G1_0 + 0.6667_1 -> G2_1;'
+                                           'G1_1 + 0.6667_0 -> G2_1;'
+                                           'G1_1 + 0.6667_1 -> G2_0;'
+
+                                           'G2_0 + G1_0 -> G3_1;'
+                                           'G2_0 + G1_1 -> G3_1;'
+                                           'G2_1 + G1_0 -> G3_1;'
+                                           'G2_1 + G1_1 -> G3_0;'
+
+                                           'G3_0 + 0.2_0 -> G4_1;'
+                                           'G3_0 + 0.2_1 -> G4_1;'
+                                           'G3_1 + 0.2_0 -> G4_1;'
+                                           'G3_1 + 0.2_1 -> G4_0'.replace('0.', 'c')
+                                                                 .replace('_0', 'a')
+                                                                 .replace('_1', 'b'),
+                                           'soloveichik2010.ts', modular=True)
+TestTubeAnalysis(polynomial, polynomialFile, True)
+polynomialFile.close()
--- build/createWinDist.sh
+++ build/createWinDist.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+
+# REMOVE OLD FILES
 rm -r build
 rm -r dist
+rm -r nuskell-0.8
+rm "nuskell-0.8.zip"
 rm "FUNDNA.spec"
 rm "FUNDNA --debug.spec"
+
+# DOWNLOAD REQUIRED FILES
 mkdir dist
 cd dist
 mkdir assets
+mkdir tests
 cd ../
 curl -o "dist/assets/result.png" https://gyazo.com/ae77ab94a7eccb8f8c6603be45aa1020.png
 curl -o "dist/assets/taylor.png" https://gyazo.com/a0c666e23c31024e4b5097f626d550e0.png
 curl -o "dist/assets/rearranged.png" https://gyazo.com/f5846f1ed0e124ad66cb5690437a8cd8.png
 
+# INSTALL REQUIRED DEPENDENCIES
 python3 -m pip install --upgrade scipy
 python3 -m pip install --upgrade networkx
 python3 -m pip install --upgrade matplotlib
@@ -22,6 +30,17 @@ python3 -m pip install --upgrade mpmath
 python3 -m pip install --upgrade schemdraw
 python3 -m pip install --upgrade pyinstaller
 
+# INSTALL NUSKELL
+curl -o "nuskell-0.8.zip" https://github.com/DNA-and-Natural-Algorithms-Group/nuskell/archive/refs/tags/v0.8.zip
+unzip nuskell-0.8.zip
+cd nuskell-0.8/nuskell-0.8
+mv * ../
+cd ../
+rm nuskell-0.8
+python3 -m pip install --upgrade dsdobjects
+python3 -m pip install .
+
+# PYINSTALL GUI EXECUTABLES
 pyinstaller gui.py Function.py FuncTypes.py GateTypes.py NotGateTypes.py RearrangeType.py Util.py --name "FUNDNA --debug" --clean --onefile --icon "assets/UK logo-white.png" --add-data "assets/*.png;assets" --hidden-import=scipy --hidden-import=networkx --hidden-import=matplotlib --hidden-import=PIL --hidden-import=PIL._imagingtk --hidden-import=PIL._tkinter_finder --hidden-import=pathlib --hidden-import=sympy --hidden-import=mpmath --hidden-import=tk --hidden-import=schemdraw --debug=imports
 pyinstaller gui.py Function.py FuncTypes.py GateTypes.py NotGateTypes.py RearrangeType.py Util.py --name "FUNDNA" --clean -w --onefile --icon "assets/UK logo-web.png" --splash "assets/FUNDNA Splash Page.png" --add-data "assets/*.png;assets" --hidden-import=scipy --hidden-import=networkx --hidden-import=matplotlib --hidden-import=PIL --hidden-import=PIL._imagingtk --hidden-import=PIL._tkinter_finder --hidden-import=pathlib --hidden-import=sympy --hidden-import=mpmath --hidden-import=tk --hidden-import=schemdraw --debug=imports
 read
