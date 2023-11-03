b1464a6491ef7744bad97da8663bda86fbc200c0
Jacksonnnnn <jackson.huse@stu.jefferson.kyschools.us>
2023-11-03 15:35:58 -0400

    Piperine installation correct <3

--- build/bash-installs/piperineInstall.sh
+++ build/bash-installs/piperineInstall.sh
@@ -10,14 +10,17 @@ python3 -m pip install --upgrade numpy scipy
 python3 -m pip install --upgrade git+https://github.com/DNA-and-Natural-Algorithms-Group/peppercompiler.git
 python3 -m pip install --upgrade git+https://github.com/DNA-and-Natural-Algorithms-Group/stickydesign.git
 echo ""
+
 echo "    *** Installing NUPACK"
 python3 -m pip install --upgrade matplotlib jupyterlab
 echo ""
 echo "Please download NUPACK 3.0.6 from https://nupack.org/downloads to your home directory (${HOME}) and then press ENTER:"
 read
-export NUPACKHOME=~/nupack3.0.6/
-cd ${NUPACKHOME} || (echo "Nupack 3.0.6 is not downloaded correctly, unable to install..." && exit)
+tar -xvf "${HOME}/nupack3.0.6.tar"
+cd ${NUPACKHOME} || (echo "NUPACK 3.0.6 is not downloaded correctly, unable to install... Please make sure NUPACK is unzipped correctly and is located at ${NUPACKHOME}" && read)
+export NUPACKHOME="${HOME}/nupack3.0.6/"
 make
-python3 -m pip install .
-python3 -m pip show nupack
-python3 -m pytest -v --pyargs nupack
+
+echo ""
+echo "    *** Installing Piperine"
+python3 -m pip install --upgrade git+https://github.com/DNA-and-Natural-Algorithms-Group/piperine.git
